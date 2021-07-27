150007ea7ecfc2b2e212d642faba67466c38a9e5
pastre <pastre68@gmail.com>
2021-07-27 19:08:25 -0300

    removes dump comment

--- morphic_text.js
+++ morphic_text.js
@@ -97,7 +97,6 @@ function doMorph() {
 }
 
 function setMorph(fraction) {
-	// fraction = Math.cos(fraction * Math.PI) / -2 + .5;
 	
 	elts.text2.style.filter = `blur(${Math.min(8 / fraction - 8, 100)}px)`;
 	elts.text2.style.opacity = `${Math.pow(fraction, 0.4) * 100}%`;
