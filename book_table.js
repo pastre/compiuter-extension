b636d87bb1c74617b42b5de324890c135c9f6717
pastre <pastre68@gmail.com>
2021-07-27 12:24:45 -0300

    Embeds animated text

--- book_table.js
+++
@@ -1,6 +0,0 @@
-const filesTable = document.getElementsByClassName("files")[0];
-const tbody = filesTable.getElementsByTagName("tbody")[0];
-
-while(tbody.childNodes.length - 1 > 2) {
-    tbody.removeChild(tbody.lastChild);
-}
